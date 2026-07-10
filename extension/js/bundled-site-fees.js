54a063b9acefde9c6e0d1c391fb9c16830a1118e
樊鹏 <fanpeng@Franks-MacBook-Pro.local>
2026-07-10 21:45:05 +0800

    update site fees (6d1d5e2de673a423)

--- extension/js/bundled-site-fees.js
+++ extension/js/bundled-site-fees.js
@@ -18,7 +18,7 @@ const BUNDLED_SITE_FEES = {
     "activity": 0.0,
     "transaction": 8.0,
     "withdrawal": 0.0,
-    "techSupport": 0.0,
+    "techSupport": 6.0,
     "commission": 22.0
   },
   "MX": {
@@ -39,7 +39,7 @@ const BUNDLED_SITE_FEES = {
     "activity": 0.0,
     "transaction": 2.24,
     "withdrawal": 0.0,
-    "techSupport": 0.0,
+    "techSupport": 6.0,
     "commission": 12.0
   },
   "SG": {
@@ -60,7 +60,7 @@ const BUNDLED_SITE_FEES = {
     "activity": 0.0,
     "transaction": 2.5,
     "withdrawal": 0.0,
-    "techSupport": 1.0,
+    "techSupport": 6.0,
     "commission": 14.0
   },
   "VN": {
